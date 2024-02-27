bc6f041e54e7e007a08556da28d180779bb271b6
Margarita Sokol <golovnina.margarita@yandex.ru>
2024-02-28 01:00:53 +0300

    added files for answering step 3.6.10

---
+++ test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(30)
+    add_to_basket_button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert add_to_basket_button.is_displayed(), "Add to basket button is unvisible"
